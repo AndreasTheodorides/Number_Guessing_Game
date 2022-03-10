b0309af2376559ed513c70c870f9460a6e1185f6
Andreas Theodoridis <antreastheodorides16@gmail.com>
2022-03-10 19:49:06 +0200

    db testing

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.File;
@@ -25,6 +27,17 @@ public class main {
                      .build();
              FirebaseApp.initializeApp(options);
              FirebaseDatabase database = FirebaseDatabase.getInstance();
+             DatabaseReference ref = database.getReference("message");
+             ref.setValue("This is a message", new DatabaseReference.CompletionListener() {
+                 @Override
+                 public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                     if (databaseError!=null){
+                         System.out.println("Data could not be saved" + databaseError.getMessage());
+                     }else{
+                         System.out.println("Data Saved Successfully");
+                     }
+                 }
+             });
          } catch (IOException e){
              e.printStackTrace();
          }
